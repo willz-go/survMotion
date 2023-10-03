09c8f2144b23f86b1134941f43e66a4eb311379c
William Gomes F. Júnior <will.junior.go@gmail.com>
2023-10-03 11:24:24 -0300

    create grid

--- main.py
+++ main.py
@@ -1,9 +1,57 @@
 import os 
 from dotenv import load_dotenv
 import cv2
+from numpy import integer
+
+GRID_SIZE = 100
+GRID_COLOR = (0, 0, 0)
+
+def calculate_grid_params(grid_size: int, img: cv2.Mat):
+    width = img.shape[1]
+    height = img.shape[0]
+
+    if grid_size > width or grid_size > height:
+         return [-1, -1]
+
+    num_cols = int(width/grid_size)
+    num_rows = int(height/grid_size)
+    return [num_rows, num_cols]
+
+def handle_mouseclicks(event, x, y, flags, params):
+    print(f'Mouse event is {event} x = {x}; y = {y}; flasg are {flags} and params are {params}')
+    pass
+
+def drawGrid(grid_size: int, img: cv2.Mat)->None:
+    grid_size = int(grid_size)
+    if not isinstance(grid_size, int):
+                grid_size = 8
+ 
+    
+    [num_rows, num_cols] = calculate_grid_params(GRID_SIZE, img) 
+    print(num_rows)
+    print(num_cols)
+    height = img.shape[0]
+    width = img.shape[1]
+    if num_cols == -1: 
+         return img
+
+    for col in range(1, num_cols +1):
+        for column_pixel in range(0, height):
+            #rgb = img[column_pixel][grid_size-1]
+            new_rgb = (0, 0, 0)
+            img[column_pixel][col*grid_size] = new_rgb
+
+    for row in range(1, num_rows +1):
+        for row_pixel in range(0, width-1):
+            #rgb = img[column_pixel][grid_size-1]
+            new_rgb = GRID_COLOR
+            img[row*grid_size][row_pixel] = new_rgb
 
 
 
+    return img
+
+    
 
 
 if __name__ == "__main__":
@@ -24,7 +72,11 @@ if __name__ == "__main__":
         print('Cannot open RTSP stream')
         exit(-1)
 
+    cv2.namedWindow("RTSP stream")
+    cv2.setMouseCallback("RTSP stream" , handle_mouseclicks)
+
     while True:
+        
         _, frame = cap.read()
         downscale_percent = 70
         width = frame.shape[1] 
@@ -34,8 +86,10 @@ if __name__ == "__main__":
         resized_frame = cv2.resize(frame, dim, interpolation = cv2.INTER_AREA)
 
         print('Resized dims are :', resized_frame.shape)
+
+        grid_frame = drawGrid(GRID_SIZE, resized_frame)
         
-        cv2.imshow('RTSP stream', resized_frame)
+        cv2.imshow('RTSP stream', grid_frame)
 
         if cv2.waitKey(1) == 27:
             break
