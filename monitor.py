b79b2a6cd2a47b27262308d6d300d303ec6be357
William Gomes F. Júnior <will.junior.go@gmail.com>
2023-10-06 00:51:48 -0300

    Core grid_diff function has been implemented and tested. Resulst are satisfatory

--- monitor.py
+++ monitor.py
@@ -1,6 +1,7 @@
 import os 
 from dotenv import load_dotenv
 import cv2
+import numpy as np
 from numpy import integer
 
 GRID_SIZE = 100
@@ -13,11 +14,35 @@ class CamMonitor:
      img_height: int = 0
      current_grid_m: int = -1
      current_grid_n: int = -1
+     threshold: int = 50 #this translates into the diference level between frames or images
 
      def __init__(self) -> None:
             self.grid_color = GRID_COLOR
             self.grid_size = GRID_SIZE
             return
+     
+     def grid_diff(self, grid_m: int, grid_n: int, frame: cv2.Mat, old_frame: cv2.Mat)-> [cv2.Mat, int]:
+         x_pos = grid_m*GRID_SIZE
+         y_pos = grid_n*GRID_SIZE #both gives the actual pixel position to start comparing
+         x_pos_max = x_pos + GRID_SIZE if x_pos + GRID_SIZE <= frame.shape[1] else frame.shape[1]  #
+         y_pos_max = y_pos + GRID_SIZE if y_pos + GRID_SIZE <= frame.shape[0] else frame.shape[0]  # both will ensure x_max and y_max are withing image boundaries
+         print(f'{x_pos} {x_pos_max} - {y_pos} {y_pos_max}')
+
+        #  grid1 = frame[x_pos:x_pos_max][y_pos:y_pos_max]
+         grid1 = frame[y_pos:y_pos_max, x_pos:x_pos_max]
+         grid2 = old_frame[y_pos:y_pos_max, x_pos:x_pos_max]
+
+         result = cv2.absdiff(grid1, grid2)
+         print(result.shape)
+
+         sum = np.sum(result) / (3*(y_pos_max-y_pos)*(x_pos_max - x_pos))
+         
+
+         print(f'Grid1 width = {grid1.shape[1]} heihgt  = {grid1.shape[0]}')
+
+         #gotta remove result as a returned value, because it won't be used later. Must return only SUM. Also update return type in function declaration after said alterations
+         return [result, sum]
+         
      
      def get_grid_position(self, x: int, y: int, width: int, height: int)->[int, int]:
          pos_x = int (x/self.grid_size)
@@ -109,6 +134,7 @@ class CamMonitor:
             grid_frame = self.drawGrid(GRID_SIZE, resized_frame)
             
             cv2.imshow('RTSP stream', grid_frame)
+        
 
             if cv2.waitKey(1) == 27:
                 break
@@ -120,6 +146,7 @@ class CamMonitor:
      def capture_img_only(self):
         load_dotenv()
         IMG_URL =  "./image/example_img.jpg"
+        IMG_URL2 =  "./image/example_img2.jpg"
         #print(f'URL  = {RTSP_URL}')
        
         
@@ -130,23 +157,34 @@ class CamMonitor:
         while True:
             
             img = cv2.imread(IMG_URL)
+            img2 = cv2.imread(IMG_URL2)
             downscale_percent = 90
             width = img.shape[1] 
             height = img.shape[0]
             dim = (int(width*(downscale_percent/100)), int(height*(downscale_percent/100)))
 
             resized_frame = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+            resized_frame2 = cv2.resize(img2, dim, interpolation = cv2.INTER_AREA)
 
             print('Resized dims are :', resized_frame.shape)
 
             grid_frame = self.drawGrid(GRID_SIZE, resized_frame)
+            grid_frame2 = self.drawGrid(GRID_SIZE, resized_frame2)
 
             if (self.current_grid_m >= 0 and self.current_grid_n >= 0):
                 grid_frame = self.paint_grid_area(self.current_grid_m, self.current_grid_n, grid_frame, (0, 255, 0))
+                grid_frame2 = self.paint_grid_area(self.current_grid_m, self.current_grid_n, grid_frame2, (0, 255, 0))
+                [grid1, diff] = self.grid_diff(self.current_grid_m, self.current_grid_n, grid_frame, grid_frame2)
+                print(f'The diff is {diff}')
+                cv2.imshow('IMG 2', grid1)
+                
             
             cv2.imshow('IMG show only', grid_frame)
+            cv2.imshow('IMG2 show only', grid_frame2)
 
-            if cv2.waitKey(1) == 27:
+            
+
+            if cv2.waitKey(500) == 27:
                 break
 
         
